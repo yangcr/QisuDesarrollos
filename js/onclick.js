87f3d19a7e6983f09f28236befde5a6241754a2b
Cristopher Yang <dg.cris.yang@gmail.com>
2021-11-24 21:51:24 -0300

    Estado en tercer preentrega

---
+++ js/onclick.js
@@ -0,0 +1,50 @@
+$(function(){
+    $('#viewiFrame1').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#iframeHolder').html('<iframe src="https://www.google.com/maps/embed?pb=!4v1635971873550!6m8!1m7!1sCAoSLEFGMVFpcE9uVnF6YXI5QkpzRmlXdlAyMWxRT0ZCc3hxV0tCU19lbGVxT1N3!2m2!1d33.86150441689807!2d-83.40859839586119!3f174.0316348685489!4f-13.524339066190791!5f0.7820865974627469" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+
+
+$(function(){
+    $('#viewiFrame2').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#iframeHolder2').html('<iframe src="https://www.google.com/maps/embed?pb=!4v1635971512614!6m8!1m7!1sCAoSLEFGMVFpcE41Tk5SUFc0MXVUVUIza3ZKdHF1X0liQTJOaUhqdm1aLWhqc2c4!2m2!1d33.57433630370327!2d-84.3520773465192!3f38.23!4f-28.39!5f0.4000000000000002" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+
+
+
+$(function(){
+    $('#viewTaiwanTour__1').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#taiwanTour__1').html('<iframe src="https://www.google.com/maps/embed?pb=!4v1636050870396!6m8!1m7!1sCAoSLEFGMVFpcE9JR1ItZ2F2SWdMeFEzekVMUGdobkJsdW1vcUZkQUVpdjBPM0Jl!2m2!1d33.916435004394!2d-84.2978801251478!3f112.34347102963832!4f-5.295055284067246!5f0.8160813932612223" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+
+
+
+$(function(){
+    $('#viewTaiwanTour__2').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#taiwanTour__2').html('<iframe src="https://www.google.com/maps/embed?pb=!4v1636050927858!6m8!1m7!1sCAoSLEFGMVFpcE9ZaTl6c1Ayc21Iblc1VUFrS2dQbm04LU9HTllqODdvY1MwcjdM!2m2!1d33.8421417!2d-84.372098!3f196.86633879496193!4f2.049999999999997!5f0.8160813932612223" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+
+$(function(){
+    $('#viewLuthierTour__1').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#LuthierTour__1').html('<iframe src="https://www.google.com/maps/embed?pb=!4v1636050792563!6m8!1m7!1sa4pxOOGtF8nT143GWgWtUA!2m2!1d34.13073102004196!2d-84.26007276546282!3f249.89!4f-4.599999999999994!5f0.8160813932612223" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+
